b881c1af2661bc9e7b166f0e719bafa90c9076fb
Nathan Graule <solarliner@gmail.com>
2020-02-25 11:37:53 +0100

    Add quad generation to main output

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,24 @@ mod parse;
 mod quads;
 
 fn main() -> anyhow::Result<()> {
+    let mut env = env::Env::new();
     let reader = Interface::new("parser")?;
     reader.set_prompt(">>> ")?;
     while let ReadResult::Input(input) = reader.read_line()? {
         match parse::parse(&input) {
-            Ok(p) => println!("{:?}", p),
-            Err(e) => eprintln!("Error! {}", e),
+            Ok(p) => {
+                println!("{:#?}", &p);
+                match quads::Quad::quad_s(p, &mut env) {
+                    Ok(q) => println!(
+                        "{}",
+                        q.into_iter()
+                            .map(|q| format!("{}\n", q))
+                            .collect::<String>()
+                    ),
+                    Err(e) => eprintln!("Quads error: {:#}", e),
+                }
+            }
+            Err(e) => eprintln!("Parsing error! {:#}", e),
         }
         reader.add_history_unique(input);
     }
